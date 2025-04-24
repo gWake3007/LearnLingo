0b6fb9638eb6f68f8d8858c2af791c18c108716d
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2025-04-24 18:52:51 +0200

    1234

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,19 @@
 import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-// import { importTeachers } from './untils/import.js';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  // useEffect(() => {
-  //   importTeachers()
-  //     .then(teachers => {
-  //       console.log('Teachers:', teachers);
-  //     })
-  //     .catch(error => {
-  //       console.error('Error:', error);
-  //     });
-  //   return () => {
-  //     console.log('Cleanup');
-  //   };
-  // }, [importTeachers]);
+  const [name, setName] = useState('Serhii');
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello {name}!</h1>
+      <input
+        type="text"
+        value={name}
+        placeholder="Please enter your Name"
+        onChange={e => setName(e.target.value)}
+      />
+      <button onClick={() => setName('')}>Clear name</button>
     </>
   );
 }
