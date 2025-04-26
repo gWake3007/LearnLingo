f52a6f43da3b6285df05e2d082d020998e5b2550
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2025-04-26 13:48:00 +0200

    123

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,1 +1,64 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import {
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth';
+
+import { auth } from '../../api/firebaseAPI.js';
+
+export const registerUser = createAsyncThunk(
+  'auth/register',
+  async ({ email, password, name }, { rejectWithValue }) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      await updateProfile(userCredential.user, { displayName: name });
+
+      return {
+        email: userCredential.user.email,
+        uid: userCredential.user.uid,
+        name: userCredential.user.displayName,
+      };
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  'auth/login',
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      return {
+        email: userCredential.user.email,
+        uid: userCredential.user.uid,
+        name: userCredential.user.displayName,
+      };
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logoutUser = createAsyncThunk(
+  'auth/logout',
+  async (_, { rejectWithValue }) => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
