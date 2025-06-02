1b1e7e9ae4c32f194bdfd0c9a456fb465d0f4c79
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2025-06-02 21:35:39 +0200

    123

--- src/components/TeacherCard/TeacherCard.jsx
+++ src/components/TeacherCard/TeacherCard.jsx
@@ -1,10 +1,13 @@
 import css from './TeacherCard.module.css';
+import { useState } from 'react';
 import { selectTeachers } from '../../redux/teachers/selectors.js';
 import { useSelector } from 'react-redux';
 
 const TeacherCard = () => {
   const teachers = useSelector(selectTeachers);
   const teacher = teachers[0];
+
+  const [isExpanded, setIsExpanded] = useState(false);
 
   if (!teacher) return null;
 
@@ -21,7 +24,9 @@ const TeacherCard = () => {
         <div className={css.titleContainer}>
           <div className={css.nameContainer}>
             <p>Languages</p>
-            <h2>{teacher.name}</h2>
+            <h2>
+              {teacher.name} {teacher.surname}
+            </h2>
           </div>
           <div className={css.infoContainer}>
             <ul className={css.list}>
@@ -32,7 +37,7 @@ const TeacherCard = () => {
                 Lessons online
               </li>
               <li>
-                Lessons done: <span>1098</span>
+                Lessons done: <span>{teacher.lessons_done}</span>
               </li>
               <li>
                 <img
@@ -40,10 +45,10 @@ const TeacherCard = () => {
                   srcSet=""
                   alt="star"
                 />
-                Rating: <span>4.8</span>
+                Rating: <span>{teacher.rating}</span>
               </li>
               <li>
-                Price / 1 hour: <span>30$</span>
+                Price / 1 hour: <span>{teacher.price_per_hour}$</span>
               </li>
             </ul>
             <button className={css.heartBtn} type="button">
@@ -55,29 +60,61 @@ const TeacherCard = () => {
           <ul>
             <li>
               <span>Speaks: </span>
-              {teacher.languages.map(()=> {}
+              {teacher.languages.join(', ')}
             </li>
             <li>
               <span>Lesson Info: </span>
-              Lessons are structured to cover grammar, vocabulary, and practical
-              usage of the language.
+              {teacher.lesson_info}
             </li>
             <li>
               <span>Conditions: </span>
-              Welcomes both adult learners and teenagers (13 years and
-              above).Provides personalized study plans
+              {teacher.conditions.join(' ')}
             </li>
           </ul>
         </div>
-        <button type="button" className={css.readMoreBtn}>
-          Read more
-        </button>
+        {!isExpanded && (
+          <button
+            type="button"
+            className={css.readMoreBtn}
+            onClick={() => setIsExpanded(true)}
+          >
+            Read more
+          </button>
+        )}
+        {isExpanded && <p className={css.experience}>{teacher.experience}</p>}
+        {isExpanded && teacher.reviews?.length > 0 && (
+          <ul className={css.commentList}>
+            {teacher.reviews.map(
+              ({ comment, reviewer_name, reviewer_rating }, idx) => (
+                <li key={idx} className={css.commentItem}>
+                  <div>
+                    <div className={css.commentImg}></div>
+                    <h4>{reviewer_name}</h4>
+                    <div>
+                      <img
+                        src="../../../public/header/star_1x.png"
+                        srcSet="../../../public/header/star_1x.png 1x, ../../../public/header/star_2x.png 2x"
+                        alt={reviewer_name}
+                      />
+                      <span>{reviewer_rating}</span>
+                    </div>
+                  </div>
+                  <p className={css.comment}>{comment}</p>
+                </li>
+              )
+            )}
+          </ul>
+        )}
         <ul className={css.lagnLevelList}>
-          <li>#A1 Beginner</li>
-          <li>#A2 Elementary</li>
-          <li>#B1 Intermediate</li>
-          <li>#B2 Upper-Intermediate</li>
+          {teacher.levels.map(level => (
+            <li>{level}</li>
+          ))}
         </ul>
+        {isExpanded && (
+          <button className={css.bookBtn} type="button">
+            Book trial lesson
+          </button>
+        )}
       </div>
     </div>
   );
